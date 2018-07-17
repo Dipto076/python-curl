69b3378d7ed9034747a4602d5b48efbd303cfe3a
Sadman Sakib <sadman.sakib@ipay.com.bd>
2018-07-17 18:10:35 +0600

    command line argument added

--- missing-invitation.py
+++ missing-invitation.py
@@ -1,28 +1,38 @@
-import requests
+import requests,csv,sys
 
-input = open('input.txt', 'r')
 success = open('success.txt', 'w')
 fail = open('fail.txt', 'w')
 
-url = 'http://10.10.10.10:8070/offer_v2/api/v1/internal/promo/activate'
+server = 'localhost'
+port = '8070'
+
+if(len(sys.argv)==3):
+	server = sys.argv[1]
+	port = sys.argv[2]
+
+url = 'http://%s:%s/offer_v2/api/v1/internal/promo/activate' % (server, port)
 headers = {'Content-Type': 'application/json'}
 
-for row in input:
-	row = row.rstrip('\n')
-	a,b = row.split(',')
-	accountId = a
-	promo = b
-	payload = '{\"accountId\": %s,\"promoCode\": %s}' % (accountId, promo)
-	response = requests.post(url, data=payload, headers=headers)
-	message = response.json()['message']
+counter = 0
+with open('valid_invitation_code_users_request.csv', newline='') as csvfile:
+	reader = csv.DictReader(csvfile)
+	for row in reader:
+		counter += 1
+		accountId = row['data_accountID']
+		promo = row['data_promoCode']
+		# print(str(accountId) + ' ' + str(promo))
+		payload = '{\"accountId\": %s,\"promoCode\": "%s"}' % (accountId, promo)
+		print(payload)
+		response = requests.post(url, data=payload, headers=headers)
+		message = response.json()['message']
 
-	result = "accountId : %s, promo : %s, message : %s\n" % (accountId, promo, message)
+		result = "row: %s, accountId : %s, promo : %s, message : %s\n" % (counter, accountId, promo, message)
 
-	print(result)
-	
-	if(message == 'Promo code activated'):
-		success.write(result)
-	else:
-		fail.write(result)
+		print(result)
+		
+		if(message == 'Promo code activated'):
+			success.write(result)
+		else:
+			fail.write(result)
 
 
